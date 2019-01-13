d330b291c9b53ffd6300c91c23858647481ab48c
halneko <otsuichi5121@gmail.com>
2019-01-13 21:06:28 +0900

    update: インデント揃え

--- DailyWorkReport/Tools/Template/src/HolidayExcelParamData.rb
+++ DailyWorkReport/Tools/Template/src/HolidayExcelParamData.rb
@@ -9,7 +9,7 @@ require File.expand_path( File.dirname(__FILE__) + '/../../lib/excel.rb' )
 # src
 # ==========================="
 class HolidayExcelParamData
-	SRC_ROOT 					= File.dirname(__FILE__) + "/.."
+	SRC_ROOT 			= File.dirname(__FILE__) + "/.."
 	PARAMETER_FILE_NAME	= "#{SRC_ROOT}/TemplateParam.xls"
 
 	public
