dd27435d87e257ac053cd9127dd5f18b612c3dcf
halneko <otsuichi5121@gmail.com>
2019-01-13 19:17:21 +0900

    clean: インデント揃え。

--- DailyWorkReport/Tools/Template/src/HolidayExcelParamData.rb
+++ DailyWorkReport/Tools/Template/src/HolidayExcelParamData.rb
@@ -11,13 +11,13 @@ require File.expand_path( File.dirname(__FILE__) + '/../../lib/excel.rb' )
 class HolidayExcelParamData
 	SRC_ROOT 					= File.dirname(__FILE__) + "/.."
 	PARAMETER_FILE_NAME	= "#{SRC_ROOT}/TemplateParam.xls"
-	
+
 	public
 	def initialize()
 		@param_list = Array.new
 		@param_list.clear
 		assertLogPrintNotFoundFile( PARAMETER_FILE_NAME )
-		
+
 		setData()
 	end
 
@@ -26,33 +26,33 @@ class HolidayExcelParamData
 	end
 
 	def setData()
-		
+
 		Excel.runDuring(false, false) do |excel|
 
 			# パラメータ用 excel を開く
 			wb_param = Excel.openWb( excel, PARAMETER_FILE_NAME )
 			ws_param = wb_param.worksheets( "祝日設定" )
-	
+
 			# 列番号の設定
 			setClumn( ws_param )
 
 			# レコードの数だけ
-			for recode in ws_param.UsedRange.Rows do 
-			
+			for recode in ws_param.UsedRange.Rows do
+
 				# 1行目はパラメータ名なのでスキップ or 空白行 or nil が入ってきた場合はスキップ
 				next if (recode.row == 1 or recode == "" or recode == nil)
-                
+
 				# パラメータを取得してpush
                 holiday               = Excel.getCellValue(ws_param, recode.row, "#{@clumn_holiday}".to_i )
 				next if ( holiday == "" or holiday == nil )
-                
+
                 param = Hash.new
                 param[:holiday] = holiday
 				@param_list.push( param )
 			end
-			wb_param.close(0)			
+			wb_param.close(0)
 		end
-		
+
 		errorCheck()
 	end
 
@@ -69,5 +69,5 @@ class HolidayExcelParamData
             assertLogPrintFalse( "#{error_str}" )
         end
 	end
-	
+
 end
