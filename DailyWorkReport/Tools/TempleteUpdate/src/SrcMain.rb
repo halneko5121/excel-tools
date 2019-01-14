4a6904e82577e3a73a79afdc37261481e9ac2436
halneko <otsuichi5121@gmail.com>
2019-01-14 15:26:15 +0900

    update: もろもろコード整理。

--- DailyWorkReport/Tools/TempleteUpdate/src/SrcMain.rb
+++ DailyWorkReport/Tools/TempleteUpdate/src/SrcMain.rb
@@ -3,16 +3,17 @@
 # ==========================="
 # require
 # ==========================="
-require File.dirname(__FILE__) + "/../../Template/src/TemplateExcelParamData.rb"
+require File.dirname(__FILE__) + "/../../Template/src/ExcelParamData.rb"
 require File.dirname(__FILE__) + "/../../lib/AppModule.rb"
-require File.dirname(__FILE__) + "/TemplateUpdateParamData.rb"
 require File.dirname(__FILE__) + "/TemplateUpdate.rb"
 
 # ==========================="
 # Const
 # ==========================="
 TITLE	= "TempleteUpdate"
-VER		= "1.0.0"
+VER		= "1.1.0"
+TAMPLATE_PARAM_FILE_NAME	= File.dirname(__FILE__) + "/../../Template/TemplateParam.xls"
+PARAMETER_FILE_NAME			= File.dirname(__FILE__) + "/../TemplateUpdateParam.xls"
 
 # ==========================="
 # src
@@ -22,14 +23,16 @@ if ( __FILE__ == $0 )
 	AppModule.main( TITLE,  VER ) {
 
 		# パラメータの設定(テンプレート)
-		template_param = TemplateExcelParamData.new
-		
+		param_hash = { name: "社員名", abbrev_name: "略名", pass: "pass", joining_time: "作成年月" }
+		template_param	= ExcelParamData.new(TAMPLATE_PARAM_FILE_NAME, "社員ごとの設定", param_hash)
+
 		# パラメータの設定
-		template_update_param = TemplateUpdateParamData.new
+		param_hash = { src_range: "旧フォーマットコピーセル範囲", dst_range: "新フォーマットペーストセル範囲" }
+		template_update_param = ExcelParamData.new(PARAMETER_FILE_NAME, "パラメータ", param_hash)
 
 		# テンプレートの書式に設定し直し
 		template_update	= TemplateUpdate.new
-		template_update.update( template_param.getStaffList(), template_update_param.getParamList() )
+		template_update.update( template_param.getParamList(), template_update_param.getParamList() )
 	}
-	
+
 end
