e3d5eae0edcdc4cc9f8b6bf0c85007bf04260f2f
halneko <otsuichi5121@gmail.com>
2019-01-18 02:40:57 +0900

    remove: 不要なコードを削除しました。

--- DailyWorkReport/Tools/TempleteUpdate/src/TemplateUpdate.rb
+++ DailyWorkReport/Tools/TempleteUpdate/src/TemplateUpdate.rb
@@ -5,7 +5,6 @@
 # ==========================="
 require File.dirname(__FILE__) + "/../../lib/excel.rb"
 require File.dirname(__FILE__) + "/../../lib/util.rb"
-require File.dirname(__FILE__) + "/Define.rb"
 
 # ==========================="
 # src
