a7ea85d14967752c854b5a70e37c1d8fff93b199
halneko <otsuichi5121@gmail.com>
2019-01-19 18:03:42 +0900

    clean: フルパスに変換するように修正しました。

--- DailyWorkReport/Tools/ProgressChecker/src/SrcMain.rb
+++ DailyWorkReport/Tools/ProgressChecker/src/SrcMain.rb
@@ -3,9 +3,9 @@
 # ==========================="
 # require
 # ==========================="
-require File.dirname(__FILE__) + "/../../lib/ExcelParamData.rb"
-require File.dirname(__FILE__) + "/../../lib/AppModule.rb"
-require File.dirname(__FILE__) + "/ProgressChecker.rb"
+require File.expand_path( File.dirname(__FILE__) + '/../../lib/AppModule.rb' )
+require File.expand_path( File.dirname(__FILE__) + '/../../lib/ExcelParamData.rb' )
+require File.expand_path( File.dirname(__FILE__) + '/ProgressChecker.rb' )
 
 # ==========================="
 # Const
