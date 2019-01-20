c7f7f78809ef095458f8d5a0db82864ad2fa16e9
halneko <otsuichi5121@gmail.com>
2019-01-20 21:55:29 +0900

    clean: 不要な

--- MonthlyWorkReport/Tools/TempleteUpdate/src/Define.rb
+++ MonthlyWorkReport/Tools/TempleteUpdate/src/Define.rb
@@ -17,12 +17,5 @@ StaffSheet = Struct.new(
 				:WorkTimeColumn)
 STAFF_SHEET_DATA = StaffSheet.new( "A", "B", "C" )
 
-IS_CHECK_SHEET_MIX			= false	# 届書チェックシートを1シートにまとめる形。
-
-if( IS_CHECK_SHEET_MIX == true )
-	ADD_CULMNS_CHECK_SHEET	= 26
-	EXT_NAME				= "xlsm"
-else
-	ADD_CULMNS_CHECK_SHEET	= 0
-	EXT_NAME				= "xlsx"
-end
+ADD_CULMNS_CHECK_SHEET	= 0
+EXT_NAME				= "xlsx"
