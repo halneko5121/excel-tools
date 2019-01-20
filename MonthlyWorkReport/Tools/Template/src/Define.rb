dfb7376e0ddc7a540a83f9068b11d48490d454f3
halneko <otsuichi5121@gmail.com>
2019-01-20 16:24:36 +0900

    remove: 不要なコードを削除しました。

--- MonthlyWorkReport/Tools/Template/src/Define.rb
+++ MonthlyWorkReport/Tools/Template/src/Define.rb
@@ -10,10 +10,3 @@ SHEET_NAME_CHECK_DESCRIPT	= "チェックシートの使い方"
 SHEET_NAME_PRORATED_TABLE	= "区分別按分表"
 SHEET_NAME_WORKS			= "業務別月報"
 SHEET_NAME_ANOTHER			= "未定"
-
-# excel のテンプレートフォーマット(スタッフシート)
-FORMAT_STAFF_SHEET_CALENDAR	= "E2:G2"	#　「年/月/期間」のセル範囲
-FORMAT_STAFF_SHEET_DAY_WEEK = "B5:B35"	# 「曜日」のセル範囲
-FORMAT_STAFF_SHEET_DATE		= "A5:A35"	# 「日付」のセル範囲
-FORMAT_STAFF_SHEET_PROJECT	= "D3:L4"	# 各種プロジェクト名のセル範囲
-FORMAT_STAFF_SHEET_ALL		= "A1:O4"	# フォーマットいろいろの各種範囲
