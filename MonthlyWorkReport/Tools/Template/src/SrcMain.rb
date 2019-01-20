7d96388c8c63a84aced4e83fe6f62487260282ed
halneko <otsuichi5121@gmail.com>
2019-01-20 15:16:01 +0900

    update: 最新の状態に更新

--- MonthlyWorkReport/Tools/Template/src/SrcMain.rb
+++ MonthlyWorkReport/Tools/Template/src/SrcMain.rb
@@ -3,15 +3,17 @@
 # ==========================="
 # require
 # ==========================="
-require File.dirname(__FILE__) + "/../../lib/AppModule.rb"
-require File.dirname(__FILE__) + "/TemplateExcelParamData.rb"
-require File.dirname(__FILE__) + "/TemplateExcelCreate.rb"
+require File.expand_path( File.dirname(__FILE__) + '/../../lib/AppModule.rb' )
+require File.expand_path( File.dirname(__FILE__) + '/../../lib/excel.rb' )
+require File.expand_path( File.dirname(__FILE__) + '/../../lib/ExcelParamData.rb' )
+require File.expand_path( File.dirname(__FILE__) + '/TemplateExcelCreate.rb' )
 
 # ==========================="
 # Const
 # ==========================="
 TITLE	= "MonthlyWorkTemplete"
 VER		= "1.1.1"
+PARAMETER_FILE_NAME	= File.dirname(__FILE__) + "/../TemplateParam.xls"
 
 # ==========================="
 # src
@@ -19,17 +21,14 @@ VER		= "1.1.1"
 if ( __FILE__ == $0 )
 
 	AppModule.main( TITLE,  VER ) {
-	
-		staff_list	= Array.new
 
-		# パラメータを取得する
-		template_param	= TemplateExcelParamData.new
-		template_param.setData()
-		staff_list		= template_param.getStaffList()
+		# パラメータを取得する(社員ごと)
+		param_hash = { id: "社員番号", name: "社員名", abbrev_name: "略名", create_calendar: "作成年月", joining_time: "入社時期", period: "月報期間" }
+		template_param = ExcelParamData.new(PARAMETER_FILE_NAME, "社員ごとの設定", param_hash)
 
 		# テンプレートのデータを元に excel の生成
 		create_excel = TemplateExcelCreate.new
-		create_excel.createExcel( staff_list )
+		create_excel.createExcel( template_param.getParamList() )
 	}
 
 end
--- MonthlyWorkReport/Tools/Template/src/TemplateExcelCreate.rb
+++ MonthlyWorkReport/Tools/Template/src/TemplateExcelCreate.rb
@@ -25,14 +25,14 @@ class TemplateExcelCreate
 	# @parm		calendar	月報日時
 	#----------------------------------------------
 	def getOutputPath( id, abbrev_name, calendar )
-	
+
 		# 数値を3桁に変換
 		staff_id	= "%03d" % id.to_i
 
-        abbrev_name= abbrev_name.encode( Encoding::UTF_8 ) 
+        abbrev_name= abbrev_name.encode( Encoding::UTF_8 )
 		file_name	= "#{staff_id}_#{abbrev_name}_1-UP作業月報_#{calendar}.#{EXT_NAME}".encode(Encoding::Windows_31J)
 		out_path	= "#{OUT_ROOT}/#{file_name}"
-		
+
 		return out_path;
 	end
 
@@ -57,9 +57,9 @@ class TemplateExcelCreate
 		ws_staff	  = wb.worksheets("開始データ")
 		ws_staff.name = "#{param_hash[:abbrev_name]}"
 		ws_staff.Cells.Item(2, 10+ADD_CULMNS_CHECK_SHEET).Value = "#{param_hash[:name]}"	# 氏名
-		
+
 		# 2013xx => [2013][xx]に分割
-		str_calendar = getSplitCalendar("#{param_hash[:create_calendar]}")
+		str_calendar = splitYearMonth("#{param_hash[:create_calendar]}")
 
 		# 平成年 / 月 を算出
 #		year_number = getYearNumber( str_calendar[0] )
@@ -79,7 +79,7 @@ class TemplateExcelCreate
 			ws_staff.range("A:Z").Locked = false
 		end
 		ws_staff.Protect
-		
+
 		#　シート保護をしない or マクロ有りブックにする
 #		ws_staff.EnableOutlining = true
 #		ws_staff.Protect( {'Contents' => true} )
@@ -93,7 +93,7 @@ class TemplateExcelCreate
 	# @parm		param_hash	パラメータを格納したハッシュ
 	#----------------------------------------------
 	def setWsParamCheckSheet( wb, param_hash )
-	
+
 		# 届書チェックシートの設定
 		ws_check = wb.worksheets("#{SHEET_NAME_CHECK}")
 		ws_check.Range("B3:B33").Value = "#{param_hash[:abbrev_name]}"
@@ -109,7 +109,7 @@ class TemplateExcelCreate
 		# 略称（ファイル名/シート名）
 		ws_propateed = wb.worksheets("#{SHEET_NAME_PRORATED_TABLE}")
 		ws_propateed.Cells.Item(5, 22).Value	= "#{param_hash[:abbrev_name]}"
-		ws_propateed.Cells.Item(5, 23).Value	= ADD_CULMNS_CHECK_SHEET		
+		ws_propateed.Cells.Item(5, 23).Value	= ADD_CULMNS_CHECK_SHEET
 		ws_propateed.Cells.Item(6, 1).Value		= ws_propateed.Cells.Item(5, 1).Value
 		ws_propateed.Cells.Item(6, 22).Value	= "#{param_hash[:abbrev_name]}"
 		ws_propateed.Cells.Columns(22).Hidden	= true
@@ -120,20 +120,20 @@ class TemplateExcelCreate
 			ws_propateed.Cells.Item(5, 1).AddComment("#{param_hash[:joining_time]}")
 		end
 	end
-	
+
 	public
 	def initialize()
 		assertLogPrintNotFoundFile( TEMPLATE_FILE_NAME )
 	end
 
 	def createExcel( staff_list )
-	
+
 		# ファイルが存在していた場合はファイルを削除
 		Dir.glob( "#{OUT_ROOT}" + "/**/" + "*.*" ) do |file_path|
 			File.delete "#{file_path}"
 		end
 
-		puts "excel count = #{staff_list.size()}"	
+		puts "excel count = #{staff_list.size()}"
 
 		Excel.runDuring(false, false) do |excel|
 
@@ -159,18 +159,18 @@ class TemplateExcelCreate
 				if( IS_CHECK_SHEET_MIX == true )
 					wb.worksheets( "届書チェックシート" ).delete()
 				end
-				
+
 				# パラメータの設定
 				setWsParamStaffSheet( wb, data )
 				setWsParamProratedTable( wb, data )
-				
+
 				# シートは非表示にしておく  / [区分別按分表][届書チェック]
 				wb.worksheets("#{SHEET_NAME_PRORATED_TABLE}").visible = false
-				
+
 				# セーブして閉じる
 				wb.save()
 				wb.close(0)
-				
+
 				# ログ用
 				puts "create excel => #{File::basename( out_path )}"
 			}
