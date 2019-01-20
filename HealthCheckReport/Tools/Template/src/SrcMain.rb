b5d9626a1c7bf852d85c7e14a5b92cb26c7c886c
halneko <otsuichi5121@gmail.com>
2019-01-20 16:58:18 +0900

    update: 更新履歴とver情報を更新しました。

--- HealthCheckReport/Tools/Template/src/SrcMain.rb
+++ HealthCheckReport/Tools/Template/src/SrcMain.rb
@@ -11,7 +11,7 @@ require File.expand_path( File.dirname(__FILE__) + "/TemplateExcelCreate.rb" )
 # Const
 # ==========================="
 TITLE	= "HealthCheckReport"
-VER		= "1.2.5"
+VER		= "1.2.6"
 PARAMETER_FILE_NAME	= File.dirname(__FILE__) + "/../TemplateParam.xls"
 
 # ==========================="
