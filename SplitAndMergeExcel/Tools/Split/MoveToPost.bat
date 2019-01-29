c29c5512b8a54bbf9c3391586c643eb302669d55
halneko <otsuichi5121@gmail.com>
2019-01-30 02:08:09 +0900

    remove: 保守しないコードを削除しました。

--- SplitAndMergeExcel/Tools/Split/MoveToPost.bat
+++
@@ -1,8 +0,0 @@
-@set RUBY_LIB_DIR=..\lib\ruby-dist
-@set SRC_DIR=.\src
-@rem 共有フォルダに接続する場合
-@set password=xxxxxxxxxx
-@set user_name=xxxxxxxxxx
-
-@%RUBY_LIB_DIR%\ruby.exe %SRC_DIR%\MoveToPostMain.rb %password% %user_name%
-pause
